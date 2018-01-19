8feb4b278862292646c516ae794e5f3ce62b21f2
Anton Bolotin <a.bolotin@pflb.ru>
2018-01-19 20:22:19 +0300

    Fix posts

--- src/test/java/ok/automation/pages/MainPage.java
+++ src/test/java/ok/automation/pages/MainPage.java
@@ -11,7 +11,31 @@ public class MainPage extends PageObject {
     @FindBy(css=".mctc_name_tx")
     private WebElementFacade userNameInHeader;
 
+    @FindBy(css="div[class=\"input_placeholder\"]")
+    private WebElementFacade postTextArea;
+
+    @FindBy(xpath="//*[@id=\"hook_Block_pfnull\"]/div/div[1]/div[1]/div[2]")
+    private WebElementFacade writePost;
+
+    @FindBy(css = "div[class=\"posting_submit button-pro\"]")
+    private WebElementFacade submitPostButton;
+
+    @FindBy(xpath="(//div[@class='feed-list']//div[@class='media-text_cnt'])[1]")
+    private WebElementFacade post;
+
     public String getUserName() {
         return userNameInHeader.getText();
+    }
+
+    public void fillPost(String text){
+        postTextArea.click();
+        writePost.sendKeys(text);
+    }
+
+    public void submitPost(){
+        submitPostButton.click();
+    }
+    public String getLastPost() {
+        return post.getText();
     }
 }

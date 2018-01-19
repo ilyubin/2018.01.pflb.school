23c161769e003e56de4442847cafdf8b29db0a81
nekoguy <worldvirusbest@rambler.ru>
2018-01-19 19:40:52 +0300

    Fixed post steps

--- src/test/java/ok/automation/pages/MainPage.java
+++ src/test/java/ok/automation/pages/MainPage.java
@@ -11,7 +11,32 @@ public class MainPage extends PageObject {
     @FindBy(css=".mctc_name_tx")
     private WebElementFacade userNameInHeader;
 
+    @FindBy(css="#hook_Block_PostingForm a[data-l*=input]")
+    private WebElementFacade postFieldHolder;
+
+    @FindBy(css=".posting_itx-w [data-initial-text-to-modify]")
+    private WebElementFacade popupPostField;
+
+    @FindBy(css=".posting_footer [data-action=submit]")
+    private WebElementFacade popupPostSubmit;
+
+    @FindBy(xpath="(//div[@class='feed-list']//div[@class='media-text_cnt'])[1]")
+    private WebElementFacade lastPost;
+
     public String getUserName() {
         return userNameInHeader.getText();
+    }
+
+    public void fillPost(String text) {
+        postFieldHolder.click();
+        popupPostField.type(text);
+    }
+
+    public void submitPost() {
+        popupPostSubmit.click();
+    }
+
+    public String getLastPost() {
+        return lastPost.getText();
     }
 }
